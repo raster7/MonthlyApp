f76e889de33f6f64a59eed1759d5cc3fed20af3d
Gleb <zakharov_glieb@mail.ru>
2023-03-05 02:11:53 +0300

    t3-5

--- project2/app/src/main/java/com/example/monthlyapp/MainActivity.java
+++ project2/app/src/main/java/com/example/monthlyapp/MainActivity.java
@@ -7,14 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.monthlyapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
     private Button btn_exit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        btn_exit = findViewById(R.id.btn_exit);
-        btn_exit.setOnClickListener(new View.OnClickListener() {
+        ActivityMainBinding view = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(view.getRoot());
+        view.btnExit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, StartActivity.class);
