b22d4ab68046443fa32d010a6de0afc801966804
Gleb <zakharov_glieb@mail.ru>
2023-03-05 02:53:12 +0300

    t4-5

--- project2/app/src/main/java/com/example/monthlyapp/MainActivity.java
+++ project2/app/src/main/java/com/example/monthlyapp/MainActivity.java
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.monthlyapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn_exit;
+    private final String TAG = "monthlyLogs";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +24,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        view.calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG,"Clicked on button 'Календарь'");
+            }
+        });
+    }
+
+    public void onClickGraph(View view) {
+        Log.i(TAG,"Clicked on button 'График'");
     }
 }
